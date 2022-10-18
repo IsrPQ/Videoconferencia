a56bf591f04a3370e819778bb25892fd3a4a766c
Jesux28 <100967921+Jesux28@users.noreply.github.com>
2022-10-18 02:37:11 -0400

    Prueba de Webcams mediante Sockets

---
+++ Videoconferencia/src/videoconferencia/VideoCliente.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoconferencia;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+/**
+ *
+ * @author Usuario
+ */
+public class VideoCliente {
+    public static void main(String args[]) throws IOException, ClassNotFoundException{
+        ServerSocket server = new ServerSocket(9999);
+        System.out.println("Esperando");
+        
+        Socket socket = server.accept();
+        System.out.println("Conectado");
+        
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        JLabel label = new JLabel();
+        JFrame frame = new JFrame();
+        frame.setSize(640,360);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        
+        label.setSize(640, 360);
+        label.setVisible(true);
+    
+        frame.add(label);
+        frame.setVisible(true);
+        while(true){
+            label.setIcon((ImageIcon)in.readObject());
+        }
+    }
+}
---
+++ Videoconferencia/src/videoconferencia/VideoSocket.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoconferencia;
+import com.github.sarxos.webcam.Webcam;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+
+
+public class VideoSocket{
+    static Socket socket;
+    public static void main(String args[]) throws IOException{
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+        socket = new Socket("localhost",9999);
+        
+        BufferedImage bm = webcam.getImage();
+        
+        ObjectOutputStream dout = new ObjectOutputStream(socket.getOutputStream());
+        ImageIcon im = new ImageIcon(bm);
+        
+        JFrame frame = new JFrame("Pc 1");
+        frame.setSize(640,360);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        
+        JLabel l = new JLabel();
+        l.setVisible(true);
+        frame.add(l);
+        frame.setVisible(true);
+        while(true){
+           bm = webcam.getImage();
+           im = new ImageIcon(bm);
+           dout.writeObject(im);
+           l.setIcon(im);
+           dout.flush();
+        }
+    } 
+}
