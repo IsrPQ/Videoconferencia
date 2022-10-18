71b0cc55ac1e4ee95a76e57f3e85a52a6aaa8d93
famil <famil@DESKTOP-4GJA5RR>
2022-10-17 22:22:30 -0400

    creacion de la interfaz principal, registro de usuario

--- Videoconferencia/src/videoconferencia/Videoconferencia.java
+++ Videoconferencia/src/videoconferencia/Videoconferencia.java
@@ -1,4 +1,4 @@
-<<<<<<< OURS
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -31,37 +31,21 @@ public class Videoconferencia {
     
     
 }
-=======
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package videoconferencia;
 
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
 /**
  *
  * @author 
  */
-public class Videoconferencia {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        new interfazAnfitrion().setVisible(true);
-        new interfazCliente().setVisible(true);
-    }
     
     
-}
->>>>>>> THEIRS
+
